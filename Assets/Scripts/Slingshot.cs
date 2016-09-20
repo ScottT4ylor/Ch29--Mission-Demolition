d1332bbc2b3088e303de533d1677eb515cc5600f
ScottT4ylor <G00236985@smail.raritanval.edu>
2016-09-20 18:58:20 -0400

    Projectile working
    The slingshot glows and will instantiate the projectile on click.

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+    public GameObject projectilePrefab;
+    public bool wtfIsThis;
+
+    private GameObject launchPoint;
+    private Vector3 launchPos;
+    private GameObject projectile;
+    private bool aimingMode;
+
+
+
+
+    void Awake()
+    {
+        launchPoint = transform.Find("LaunchPoint").gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPoint.transform.position;
+    }
+	
+	void Start () {
+	
+	}
+	
+
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(projectilePrefab) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+}
