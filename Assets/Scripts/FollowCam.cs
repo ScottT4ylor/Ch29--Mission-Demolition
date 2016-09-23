b06a4153036fadbece80b9d71e0e70041e0fd5d4
ScottT4ylor <G00236985@smail.raritanval.edu>
2016-09-22 21:59:26 -0400

    Added FollowCam script
    Camera follows the projectile as it flies.

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+    static public FollowCam S;
+
+    public bool wtfIsThis2;
+
+
+    public GameObject poi;
+    public float camZ;
+
+    void Awake()
+    {
+        S = this;
+        camZ = transform.position.z;
+    }
+
+    void Update()
+    {
+        if (poi == null) return;
+
+        Vector3 destination = poi.transform.position;
+        destination.z = camZ;
+        transform.position = destination;
+    }
+}
