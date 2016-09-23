773beb80f1d4ecd2c1004f65a9fc2fac867a2d5a
ScottT4ylor <G00236985@smail.raritanval.edu>
2016-09-22 22:40:33 -0400

    FollowCam updated
    Smoothed out movement and made it follow the projectile based on speed
    and height, while keeping the ground within view.

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class FollowCam : MonoBehaviour {
     static public FollowCam S;
-
+    public int delay = 25;
+    public float ease = 0.05f;
     public bool wtfIsThis2;
 
 
+    private Vector3 lookAhead;
+    private Vector2 minXY = new Vector2(0, 0);
     public GameObject poi;
-    public float camZ;
+    private float camZ;
 
     void Awake()
     {
@@ -16,12 +19,28 @@ public class FollowCam : MonoBehaviour {
         camZ = transform.position.z;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (poi == null) return;
-
-        Vector3 destination = poi.transform.position;
+        if (delay > 0 && poi.transform.position.x < 0 && poi.transform.position.y < 7)
+        {
+            delay--;
+            return;
+        }
+        delay = 0;
+        lookAhead = poi.transform.position;
+        lookAhead.x += poi.GetComponent<Rigidbody>().velocity.x * 0.75f;
+        lookAhead.y += poi.GetComponent<Rigidbody>().velocity.y * 0.5f;
+        Vector3 destination = Vector3.Lerp(transform.position, lookAhead, ease);
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
         destination.z = camZ;
+        GetComponent<Camera>().orthographicSize = destination.y + 10;
         transform.position = destination;
+    }
+
+    void returnToSlingshot()
+    {
+        //because it has to return to the slingshot eventually.
     }
 }
